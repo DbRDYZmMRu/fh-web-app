2a3fd50111ea53c578da5257679c449ed6e59d90
DbRDYZmMRu <frithmangrove@gmail.com>
2025-04-18 20:19:33 +0100

    update

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 import { createApp } from "./lib/petite-vue.js";
-import { initDiv } from "./runtime/app/initDiv.js";
 import { initializeApp } from "./runtime/app/init.js";
-
-// Dynamically create the #app div
-initDiv();
 
 // Initialize the app
 const appConfig = initializeApp();
--- runtime/app/init.js
+++ runtime/app/init.js
@@ -1,21 +1,16 @@
 import { store } from "../../store/index.js";
+import { initDiv } from "./initDiv.js";
 import { initializeRouter } from "../../router/index.js";
 
 // Function to initialize the app's global state
 export function initializeApp() {
+  // Dynamically create the #app div
+  initDiv();
+  
   const router = initializeRouter(); // Initialize the router
   
   return {
     store, // Attach the store to the app
     router, // Attach the router to the app (optional, for advanced use)
-    Counter(props) {
-      return {
-        $template: "#counter-template",
-        count: props.initialCount,
-        inc() {
-          this.count++;
-        },
-      };
-    },
   };
 }
