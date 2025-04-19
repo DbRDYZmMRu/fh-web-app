c276baac23df7c6f84d18c69639065ce88cb2aa7
DbRDYZmMRu <frithmangrove@gmail.com>
2025-04-19 14:32:21 +0100

    Update initDiv.js

--- runtime/app/initDiv.js
+++ runtime/app/initDiv.js
@@ -1,12 +1,54 @@
 import { store } from "../../global.js";
 export function initDiv() {
-  const appDiv = document.createElement('div');
-  appDiv.id = 'app';
+  const mainDiv = `  
   
-  const paragraph = document.createElement('p');
-  paragraph.textContent = 'Hello World';
+   <header>
+    <h1>Music & Poetry App</h1>
+    <nav>
+      <button @click="path('/')">Home</button>
+      <button @click="path('/musicpool')">Music</button>
+      <button @click="path('/reader')">Reader</button>
+      <button @click="path('/home')">Home II</button>
+      <button @click="path('/upcoming')">Upcoming</button>
+      <button @click="path('/your')">Undefined</button>      
+    </nav>
+  </header>
+
   
-  appDiv.appendChild(paragraph);
+  <p>{{store.someValue}} is okay</p>
+  <p>{{store}} is okay</p>
+  <input v-model="store.someValue" type="text" name="" id="" value="" />
   
-  document.body.appendChild(appDiv);
+  <p @click="store.getDate()">click here</p>
+  
+  <main>
+    <div v-if="store.route === 'home'">
+      <h2>Welcome to the Home Page</h2>
+    </div>
+    <div v-if="store.route === 'music'">
+      <h2>Music Section</h2>
+    </div>
+    <div v-if="store.route === 'poetry'">
+      <h2>Poetry Section</h2>
+    </div>
+    <div v-if="store.route === 'reader'">
+      <h2>Reader Section</h2>
+    </div>
+    <div v-if="store.route === 'upcoming'">
+      <h2>upcoming Page</h2>
+    </div>
+  </main>
+  
+    <p>Main App: IP Access is {{ store.IPAccess ? 'Enabled' : 'Disabled' }}</p>
+  <p>{{ store.workPlan }}</p>
+  <button @click="store.toggleIPAccess()">Toggle IP Access</button>
+  
+  
+  
+
+
+  `;
+  
+  
+  document.body.innerHTML += mainDiv;
 }
