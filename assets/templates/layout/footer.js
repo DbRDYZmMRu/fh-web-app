cfe8bd50931a70b2f2555833f40f89440d7c4aa6
DbRDYZmMRu <frithmangrove@gmail.com>
2025-05-07 19:04:59 +0100

    update

--- assets/templates/layout/footer.js
+++ assets/templates/layout/footer.js
@@ -1,98 +1,85 @@
-export const footer = `
-
-    <a id="designer" href="https://www.hilton.com/en/p/for-the-stay/" target="_blank">
-    <img class="img-fluid" src="../../images/ads/Hilton.jpg" alt="Hilton For The Stay">
-  </a>
-    
-    <
-    section class = "py-5 bg-pink-gradient" >
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-12 text-center">
-          <h3 class="text-white" id="some-id">
-            I'm here
-            <u class="typer text-warning" data-words="writing,making music," data-delay="100" data-deletedelay="1000" style="color: black"></u>
-            often.
-          </h3>
-        </div>
-      </div>
-    </div> <
-    /section>
-
-    
-    <footer class="footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 position-relative mb-5 mb-sm-0">
-          <div class="position-absolute top-0 start-0 slice-pt ps-5 ps-lg-8"></div>
-        </div>
-        <div class="col-lg-6">
-
-          <div class="text-center slice-ptb">
-            <h4 class="mb-4 mt-5 mt-lg-0">Time Open</h4>
-            <div class="mb-4">
-              <h5>Monday to Friday</h5>
-              <p>8:30 am to 5 pm</p>
-            </div>
-            
-            <h4 class="mb-4">Address</h4>
-            <div class="mb-4">
-              <h5>11, Akindenoh street Ota, Ogun State.</h5>
-              <p><a href="mailto:info@frithhilton.com.ng">Send Frith Hilton Mail</a></p>
-              
-            </div>
-            <h2 class="text-green mb-0 mt-5">
-              <i class="feather icon-phone-call"></i> (234) 815 813 2408
-            </h2>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-    
-    <
-    footer class = "footer bg-black slice-pt" >
-    <div class="container">
-      <div class="row">
-        <div>
-          <h2 class="text-white display-2 mb-4">
-            <span class="typer text-white" id="some-id" data-words="Catch up!" data-delay="100">
-            </span>
-          </h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-6 col-lg-4 offset-lg-2">
-          <div class="social-icons icons-xl mt-5">
-            <a class="icon text-white" href="https://youtube.com/@frithhilton17?si=8pEsa1wf1KxLNvBK" target="_blank">
-              <i class="fab fa-youtube"></i>
-            </a>
-            <a class="icon text-white" href="https://www.tiktok.com/@frithhilton17" target="_blank">
-              <i class="fab fa-tiktok"></i>
-            </a>
-            <a class="icon text-white" href="https://www.twitter.com/frithhilton17" target="_blank">
-              <i class="fab fa-x-twitter"></i>
-            </a>
-            <a class="icon text-white" href="https://www.instagram.com/frithhilton17" target="_blank">
-              <i class="fab fa-instagram"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-      <hr class="mt-7" />
-      <div class="row text-center">
-        <div class="col-12">
-          <div class="developer-footer mb-2">
-            <img src="images/Leondev.png" alt="lEoNDɘv" width="80" height="80">
-          </div>
-          <p class="text-white mt-3 mb-4">
-            © <span id="getFullYear"></span>
-            <span class="text-pink-gradient">Howard Frith Hilton</span>
-          </p>
-        </div>
-      </div>
-    </div> <
-    /footer>
-    
-
-`;
+  
+  export const footer = `
+  
+  
+		<section class="py-5 bg-pink-gradient">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-12 text-center">
+						<h3 class="text-white" id="some-id">
+							I'm here
+							<u class="typer text-warning" data-words="writing,making music," data-delay="100" data-deletedelay="1000" style="color: black;"></u>
+							often.
+						</h3>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 position-relative mb-5 mb-sm-0">
+						<div class="position-absolute top-0 start-0 slice-pt ps-5 ps-lg-8"></div>
+					</div>
+					<div class="col-lg-6"></div>
+				</div>
+			</div>
+		</footer>
+		<footer class="footer bg-black slice-pt">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-lg-4 offset-lg-2">
+						<h2 class="text-white display-2 mb-4">
+							<span class="typer text-white" id="some-id" data-words="Catch up!" data-delay="100">
+							</span>
+						</h2>
+						<div class="social-icons icons-xl mt-5">
+							<a class="icon text-white" href="https://youtube.com/@frithhilton17?si=8pEsa1wf1KxLNvBK" target="_blank">
+								<i class="fab fa-youtube"></i>
+							</a>
+							<a class="icon text-white" href="https://www.tiktok.com/@frithhilton17" target="_blank">
+								<i class="fab fa-tiktok"></i>
+							</a>
+							<a class="icon text-white" href="https://www.twitter.com/frithhilton17" target="_blank">
+								<i class="fab fa-x-twitter"></i>
+							</a>
+							<a class="icon text-white" href="https://www.instagram.com/frithhilton17" target="_blank">
+								<i class="fab fa-instagram"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+				<hr class="mt-7">
+				<div class="row text-center">
+					<div class="col-12">
+						<div class="developer-footer mb-2">
+							<a href="http://leondev.com.ng" target="_blank">
+								<img src="/images/Leondev.png" alt="lEoNDɘv" width="80" height="80">
+							</a>
+						</div>
+						<p class="text-white mt-3 mb-4">©
+							<span id="getFullYear"></span>
+							<b>|</b>
+							Managed by
+							<span class="text-pink-gradient">
+								Frith Nightswan Enterprises</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<div class="go-top">
+			<p class="go-top-text">Scroll to top
+				<i class="fas fa-long-arrow-alt-right ps-2"></i>
+			</p>
+		</div>
+		<div class="col-lg-3 col-sm-6">
+			<p @click="window.location.href='../search.html'" class="btn btn-naked-wrap search-btn">
+				<i class="fas fa-search"></i>
+				<span>
+					search
+				</span>
+			</p>
+		</div>
+  
+  `;
--- assets/templates/layout/header.js
+++ assets/templates/layout/header.js
@@ -3,47 +3,75 @@ import { store } from "../../../global.js";
 
 export const header = `
   
-  <header class="header">
+		<header class="header">
+			<nav class="navbar navbar-light navbar-expand-lg navbar-default">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="/">
+						<img class="logo" src="/images/logo.png" alt="Frith Hilton Logo">
+					</a>
+					<button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar top-bar mt-0"></span>
+						<span class="icon-bar middle-bar"></span>
+						<span class="icon-bar bottom-bar"></span>
+					</button>
+					<div class="collapse navbar-collapse justify-content-center" id="navbarCollapse">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link" href="/">
+									<span class="text-line">
+										Home
+									</span>
+								</a>
+							</li>
+							<li class="nav-item dropdown active">
+								<a @click="store.goToPublished" class="nav-link dropdown-toggle" href="#" id="navbar-pages" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<span class="text-line">
+										Publications</span>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="../../pages/upcoming.html">
+									<span class="text-line">
+										Upcoming Poetry</span>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="../../pages/musicpool.html">
+									<span class="text-line">
+										Musicpool
+									</span>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="pages/bio.html">
+									<span class="text-line">
+										Bio
+									</span>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="../../cookies.html">
+									<span class="text-line">
+										Cookies Policy
+									</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="navbar-icons list-unstyled mb-0 ms-auto">
+						<a class="icon" href="https://www.facebook.com/frithhilton17" target="_blank">
+							<i class="feather icon-facebook"></i>
+						</a>
+						<a class="icon" href="https://www.instagram.com/frithhilton17" target="_blank">
+							<i class="feather icon-instagram"></i>
+						</a>
+						<a class="icon" href="https://www.twitter.com/frithhilton17" target="_blank">
+							<i class="feather icon-twitter"></i>
+						</a>
+					</div>
+				</div>
+			</nav>
+		</header>
 
-    <nav class="navbar navbar-light navbar-expand-lg navbar-default">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.html"><img class="logo" src="images/logo.png" alt="Frith Hilton Logo" /></a>
-        
-        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="icon-bar top-bar mt-0"></span>
-          <span class="icon-bar middle-bar"></span>
-          <span class="icon-bar bottom-bar"></span>
-        </button>
-        
-        <div class="collapse navbar-collapse justify-content-center" id="navbarCollapse">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.html"><span class="text-line"> Home </span></a>
-            </li>
-            <li class="nav-item dropdown">
-              <a @click="store.goToPublished" class="nav-link dropdown-toggle" href="#" id="navbar-pages" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="text-line"> Publications</span>
-              </a>
-            </li>
-            
-            
-            <
-            li class = "nav-item" >
-            <a class="nav-link" href="pages/upcoming.html"><span class="text-line"> Upcoming Poetry</span></a> <
-            /li> <
-            li class = "nav-item" >
-            <a class="nav-link" href="pages/musicpool.html"><span class="text-line"> Musicpool </span></a> <
-            /li> <
-            li class = "nav-item" >
-            <a class="nav-link" href="pages/bio.html"><span class="text-line"> Bio </span></a> <
-            /li> <
-            li class = "nav-item" >
-            <a class="nav-link" href="cookies.html"><span class="text-line"> Cookies Policy </span></a> <
-            /li> <
-            /ul> <
-            /div> 
-              </div> <
-              /nav> 
-              </header>
 
 `;
